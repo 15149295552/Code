ec3a03176461cef397d8cc523d775edc4eedbb3a
包l袱 <2940411390@qq.com>
2022-11-23 22:15:58 +0800

    no message

---
+++ Month06/day01/01_numpy.py
@@ -0,0 +1,36 @@
+'''
+numpy中,数组的创建
+'''
+import numpy as np
+
+#array
+ary = np.array([1,2,3,4,5,6])
+# print(ary)
+# print(type(ary))
+
+# print(ary * 2)
+# print(ary == 3)
+# print(ary + ary)
+# print(ary * ary)
+
+ary = np.array([[1,2,3],
+                [4,5,6]])
+# print(ary)
+
+
+#arange(能够生成浮点数)
+ary = np.arange(0,1.1,0.1)
+# print(ary)
+
+
+#zeros
+ary = np.zeros(10,dtype='int32')
+ary = np.zeros((3,3),dtype='int32')
+ary = np.zeros((2,2,2),dtype='int32')
+# print(ary)
+
+
+#ones
+ary = np.ones(10,dtype='int32')
+print(ary)
+
